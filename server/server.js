effaa4850729aafb77f9d885a7b4b9ae728cc9ca
Gary Murphy <compsciguy.it@gmail.com>
2018-12-02 13:59:54 +1000

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -30,7 +30,6 @@ app.get('/todos', (req, res) => {
     });
 });
 
-// GET /todos/12345
 app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
 
@@ -42,7 +41,7 @@ app.get('/todos/:id', (req, res) => {
         if(!todo) {
             return res.status(404).send();
         } 
-        
+
         res.send({todo});
     }).catch((e) => {
         res.status(400).send();
