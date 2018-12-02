f9e0c5ba225d65dcf54388c1b0707365fe901299
Gary Murphy <compsciguy.it@gmail.com>
2018-12-02 15:29:26 +1000

    Setup app for heroku Heroku is asking for credit card details created account on mlab.com which will hopefully work out better

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,11 @@ var mongoose = require('mongoose');
 // Tell mongoose to use the built-in promise library
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+let db = {
+    localhost: 'mongodb://localhost:27017/TodoApp',
+    mlab: 'mongodb://<dbuser>:<dbpassword>@ds123584.mlab.com:23584/tranquil-brushlands-32286'
+};
+
+mongoose.connect(db.localhost || db.mlab, { useNewUrlParser: true });
 
 module.exports = {mongoose};
