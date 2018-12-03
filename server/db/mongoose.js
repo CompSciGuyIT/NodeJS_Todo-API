eb7e4e62582f09c19c4620d56c8e62ae4d774c8f
Gary Murphy <compsciguy.it@gmail.com>
2018-12-03 13:11:43 +1000

    Reverted to course version mlab.com didn't work out

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,11 +3,6 @@ var mongoose = require('mongoose');
 // Tell mongoose to use the built-in promise library
 mongoose.Promise = global.Promise;
 
-let db = {
-    localhost: 'mongodb://localhost:27017/TodoApp',
-    mlab: 'mongodb://<dbuser>:<dbpassword>@ds123584.mlab.com:23584/tranquil-brushlands-32286'
-};
-
-mongoose.connect(db.localhost || db.mlab, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 
 module.exports = {mongoose};
